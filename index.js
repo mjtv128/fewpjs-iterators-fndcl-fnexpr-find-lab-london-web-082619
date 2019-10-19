c02d7cbf13d7e4e56eb392d341e73be7c5d72033
Michelle Van <mjtvan@gmail.com>
2019-10-19 18:07:49 +0100

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let result =  array.find( obj => obj.result === "W" );
+  if (result){
+    return result.year
+  }else {
+    return undefined
+  };
+}
